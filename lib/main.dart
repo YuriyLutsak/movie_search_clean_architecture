96c31e33674346effb78903b876d52c7a26aa021
Yuriy <lyn81184@gmail.com>
2024-08-19 09:18:34 +0300

    add hive in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:movie_search/dependency_injection.dart';
 import 'package:movie_search/domain/entity/hive/movie_hive/movie_hive.dart';
+import 'package:path_provider/path_provider.dart';
 
-import 'movie_serch_app.dart';
+import 'movie_serch_app.dart'; // Для получения пути к документам
 
 void main() async {
-  Hive.registerAdapter(MovieHiveAdapter());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final appDocumentDir = await getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDir.path); // Инициализация Hive с указанием пути
+
+  Hive.registerAdapter(MovieHiveAdapter()); // Регистрация адаптера
   setup();
+
   runApp(MyApp());
 }
